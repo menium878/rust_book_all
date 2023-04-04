8b8170db85beb116cee321bac51e77da802cfa65
unknown <meus13@gmail.com>
2023-04-04 23:36:36 +0200

    Generate random between 1..=100

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,12 @@
 use std::io;
+
+use rand::Rng;
 fn main() {
     println!("Guess the number!");
-
+    let secret_number =rand::thread_rng().gen_range(1..=100);
     println!("Please input your guess.");
 
     let mut guess=String::new();
     io::stdin().read_line( &mut guess).expect("Couldn't grab the input");
-    println!("Guessed number {guess}");
+    println!("Guessed number {guess} and rand is {secret_number}");
 }
