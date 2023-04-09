01ac2ae1530e09b2388409daa1f0e72f5a654505
unknown <meus13@gmail.com>
2023-04-09 21:52:44 +0200

    obsluga invalid type

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -12,7 +12,10 @@ fn main() {
     let mut guess=String::new();
     io::stdin().read_line( &mut guess).expect("Couldn't grab the input");
     println!("You guessed: {guess}");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+    };
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
