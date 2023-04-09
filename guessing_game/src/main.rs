91541f8b56a4c13a5ce128960f963e1925551a7d
unknown <meus13@gmail.com>
2023-04-09 20:41:06 +0200

    opis

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,13 @@
 use std::io;
 
 use rand::Rng;
-use std::cmp::Ordering;
+use std::cmp::Ordering; // great for cmp - comparison
 
 fn main() {
     println!("Guess the number!");
     let secret_number =rand::thread_rng().gen_range(1..=100);
     println!("Please input your guess.");
-
+    
     let mut guess=String::new();
     io::stdin().read_line( &mut guess).expect("Couldn't grab the input");
     println!("You guessed: {guess}");
